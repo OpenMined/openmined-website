feae23bd1c362476489d4e2c3d2c137fd339a02f
cereallarceny <me@patrickcason.com>
2018-01-21 18:32:26 +0800

    Working for most pages

--- server/universal.js
+++ server/universal.js
@@ -41,37 +41,37 @@ const isBot = userAgent => {
 };
 
 const universalLoader = async (req, res) => {
-  try {
-    console.log('NOW ENTERING HERE', 1);
-    const browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-
-    console.log('NOW ENTERING HERE', 2);
-
-    await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
-    );
-
-    console.log('NOW ENTERING HERE', 3);
-
-    await page
-      .goto('http://localhost:' + PORT + req.originalUrl, {
-        waitUntil: 'networkidle0'
-      })
-      .then(response => {
-        console.log('NOW ENTERING HERE', 4, response);
-      });
-
-    const html = await page.evaluate(() => {
-      return document.documentElement.innerHTML;
-    });
-
-    console.log('NOW ENTERING HERE', html);
-
-    res.send(html);
-  } catch (e) {
-    res.send('ERROR');
-  }
+  // try {
+  //   console.log('NOW ENTERING HERE', 1);
+  //   const browser = await puppeteer.launch({ headless: true });
+  //   const page = await browser.newPage();
+  //
+  //   console.log('NOW ENTERING HERE', 2);
+  //
+  //   await page.setUserAgent(
+  //     'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'
+  //   );
+  //
+  //   console.log('NOW ENTERING HERE', 3);
+  //
+  //   await page
+  //     .goto('http://localhost:' + PORT + req.originalUrl, {
+  //       waitUntil: 'networkidle0'
+  //     })
+  //     .then(response => {
+  //       console.log('NOW ENTERING HERE', 4, response);
+  //     });
+  //
+  //   const html = await page.evaluate(() => {
+  //     return document.documentElement.innerHTML;
+  //   });
+  //
+  //   console.log('NOW ENTERING HERE', html);
+  //
+  //   res.send(html);
+  // } catch (e) {
+  //   res.send('ERROR');
+  // }
   // if (!isBot(req.headers['user-agent'])) {
   //   const filePath = path.resolve(__dirname, '../build/index.html');
   //
@@ -107,45 +107,49 @@ const universalLoader = async (req, res) => {
   //   }
   // }
 
-  // // Load in our HTML file from our build
-  // const filePath = path.resolve(__dirname, '../build/index.html');
-  //
-  // fs.readFile(filePath, 'utf8', (err, htmlData) => {
-  //   // If there's an error... serve up something nasty
-  //   if (err) {
-  //     console.error('Read error', err);
-  //
-  //     return res.status(404).end();
-  //   }
-  //
-  //   // Create a store and sense of history based on the current path
-  //   const { store, history } = createServerStore(req.path);
-  //
-  //   // Render App in React
-  //   const routeMarkup = renderToString(
-  //     <Provider store={store}>
-  //       <ConnectedRouter history={history}>
-  //         <Route component={App} />
-  //       </ConnectedRouter>
-  //     </Provider>
-  //   );
-  //
-  //   // Let Helmet know to insert the right tags
-  //   const helmet = Helmet.renderStatic();
-  //
-  //   // Form the final HTML response
-  //   const html = prepHTML(htmlData, {
-  //     html: helmet.htmlAttributes.toString(),
-  //     head:
-  //       helmet.title.toString() +
-  //       helmet.meta.toString() +
-  //       helmet.link.toString(),
-  //     body: routeMarkup
-  //   });
-  //
-  //   // Up, up, and away...
-  //   res.send(html);
-  // });
+  console.log('Are we a bot?', isBot(req.headers['user-agent']));
+
+  // Load in our HTML file from our build
+  const filePath = path.resolve(__dirname, '../build/index.html');
+
+  fs.readFile(filePath, 'utf8', (err, htmlData) => {
+    // If there's an error... serve up something nasty
+    if (err) {
+      console.error('Read error', err);
+
+      return res.status(404).end();
+    }
+
+    // Create a store and sense of history based on the current path
+    const { store, history } = createServerStore(req.path);
+
+    // Render App in React
+    const routeMarkup = renderToString(
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Route component={App} />
+        </ConnectedRouter>
+      </Provider>
+    );
+
+    // Let Helmet know to insert the right tags
+    const helmet = Helmet.renderStatic();
+
+    console.log('META TAGS', helmet.meta.toString());
+
+    // Form the final HTML response
+    const html = prepHTML(htmlData, {
+      html: helmet.htmlAttributes.toString(),
+      head:
+        helmet.title.toString() +
+        helmet.meta.toString() +
+        helmet.link.toString(),
+      body: routeMarkup
+    });
+
+    // Up, up, and away...
+    res.send(html);
+  });
 };
 
 export default universalLoader;
