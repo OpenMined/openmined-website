56a843f24f3aaaec1795ee84a645bfefddb48dd6
cereallarceny <me@patrickcason.com>
2018-06-07 14:30:40 -0300

    Potentially fixed image loading on ssr

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
-require('ignore-styles');
+const md5File = require('md5-file');
+const path = require('path');
+
+// Ignore those pesky styles
+const ignoreStyles = require('ignore-styles');
+const register = ignoreStyles.default;
+const extensions = ['.gif', '.jpeg', '.jpg', '.png', '.svg'];
+
+register(ignoreStyles.DEFAULT_EXTENSIONS, (mod, filename) => {
+  const ext = extensions.find(f => filename.endsWith(f));
+  if (!ext) return ignoreStyles.noOp();
+
+  const hash = md5File.sync(filename).slice(0, 8);
+  const bn = path.basename(filename).replace(/(\.\w{3})$/, `.${hash}$1`);
+  mod.exports = `/static/media/${bn}`;
+});
 
 require('babel-register')({
   ignore: /\/(build|node_modules)\//,
