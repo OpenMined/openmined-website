8008a6d5bb02b9dc39a9210f7c3a104e4ca156ee
Patrick Cason <me@patrickcason.com>
2020-06-25 16:52:04 +0100

    Removing covid dialog

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -9,7 +9,6 @@ import FullStory from 'react-fullstory';
 import { removeNotification } from '../../modules/notifications';
 
 // UI Components
-import Dialog from './components/dialog';
 import Notifications from './components/notifications';
 import Header from './components/header';
 
@@ -21,13 +20,8 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    if (localStorage.getItem('covid-dialog') === null) {
-      localStorage.setItem('covid-dialog', true);
-    }
-
     this.state = {
       shouldShowHeader: false,
-      showingCovidModal: localStorage.getItem('covid-dialog') === 'true',
     };
   }
 
@@ -49,53 +43,8 @@ class App extends Component {
   }
 
   render() {
-    const covidCopy = {
-      title: 'Help us empower COVID-19 apps with privacy',
-      content: (
-        <>
-          <p>
-            <b>Privacy is a human right.</b> Fighting the Coronavirus pandemic
-            is a global responsibility, but that doesn't mean app developers
-            should compromise the safety, privacy, and security of their users.
-          </p>
-          <p>
-            OpenMined is providing <b>free open-source support</b> for all
-            COVID-19 app developers to protect their user's data privacy
-            properly. We are seeking to assist all developers fighting the
-            Coronavirus pandemic to augment their approach with
-            privacy-preserving techniques.
-          </p>
-        </>
-      ),
-      cta: 'Ready to join the fight?',
-      buttons: [
-        {
-          text: 'Join our mission',
-          color: 'primary',
-          link:
-            'https://blog.openmined.org/providing-opensource-privacy-for-covid19/',
-        },
-        {
-          text: 'Read our roadmap',
-          color: 'light-gray',
-          link: 'https://blog.openmined.org/covid-app-privacy-advice/',
-        },
-      ],
-    };
-
     return (
       <div id="app">
-        <Dialog
-          overlay
-          {...covidCopy}
-          isOpen={this.state.showingCovidModal}
-          toggle={() => {
-            const newState = !this.state.showingCovidModal;
-
-            localStorage.setItem('covid-dialog', newState);
-            this.setState({ showingCovidModal: newState });
-          }}
-        />
         <Notifications
           notifications={this.props.notifications}
           removeFunc={this.props.removeNotification}
