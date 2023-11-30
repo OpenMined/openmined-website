48b41672489c61944653e56e54195142891eaee7
Thiago Costa Porto <thiagocostaporto@gmail.com>
2023-11-30 18:45:46 +0100

    feat: add routes

---
+++ src/routes/SWE.jsx
@@ -0,0 +1,251 @@
+import LinkButton from '../components/LinkButton';
+
+export default () => {
+  return (
+    <section className="flex flex-col px-6 max-w-[1152px] mx-auto mb-6">
+      <h2 className="font-bold mb-2">Senior Software Engineer (Remote)</h2>
+      <p>
+        <strong>Location:</strong> Global Remote
+      </p>
+      <p>
+        <strong>Department:</strong> Engineering
+      </p>
+
+      <br />
+      <p>
+        <strong>
+          Want to help democratize access to cutting edge AI Research, but don't
+          want to move to Silicon Valley?
+        </strong>
+      </p>
+
+      <p>
+        Do you have a track record of exceeding expectations in your previous
+        roles?
+      </p>
+      <p>
+        Are you a highly talented individual with hard won industry experience?
+      </p>
+      <p>Are you mission driven and looking for a role with high impact?</p>
+      <br />
+
+      <p>
+        Ready to level up and do the most challenging and rewarding thing in
+        your career so far?
+      </p>
+      <br />
+
+      <p>
+        <strong>NOTE:</strong> This role is evergreen. We are hiring now and
+        will continue to hire as we grow. Imposter syndrome is real, even if you
+        don't believe you are qualified, but your heart says this job is for
+        you, please apply anyway, what do you have to lose?
+      </p>
+      <br />
+
+      <LinkButton href="/careers/software-engineer-form">Apply Now!</LinkButton>
+
+      <br />
+      <h3 className="text-2xl">About OpenMined</h3>
+
+      <p>
+        Led by Andrew Trask (author of Grokking Deep Learning), the team at
+        OpenMined is made of a unique blend of PhD candidates, graduates,
+        autodidacts and ex-Big Tech / AI startup talent.
+      </p>
+      <br />
+      <p>
+        At OpenMined, we are committed to creating a profound and positive
+        impact on society by changing the current paradigm of computation.
+      </p>
+      <br />
+      <p>
+        Our flagship Python framework PySyft allows users to perform data
+        science on data that is hosted remotely, without obtaining a copy. We
+        leverage a combination of open source technologies and cryptographic
+        algorithms to provide a way to answer society's most important questions
+        while protecting the privacy of individuals.
+      </p>
+      <br />
+      <p>
+        OpenMined has a successful track record of executing high-impact
+        projects and collaborations that span the globe. We have partnered with
+        companies and organizations such as Meta, PyTorch, Twitter, Google,
+        Microsoft, LinkedIn, the United Nations and the governments of the
+        United States, United Kingdom, France, and New Zealand.
+      </p>
+      <br />
+      <p>
+        Most recently the UK Frontier AI TaskForce (AI Safety Institute)
+        announced their partnership with OpenMined to “develop and deploy
+        technical infrastructure that will facilitate AI safety research across
+        governments and AI research organisations”.
+      </p>
+      <br />
+      <p>
+        We are seeking passionate Software Engineers to join our talented team
+        of mission driven engineers, data scientists and product designers.
+      </p>
+      <br />
+
+      <h3 className="text-2xl">Your Role</h3>
+      <p>
+        You are a seasoned engineer with an understanding of data driven
+        workflows in distributed systems and containerized applications. Your
+        expertise spans various programming paradigms, and you have a rich
+        experience in multiple programming languages, web technologies, and data
+        science / ML tools. While AI / ML knowledge is a plus, your primary
+        focus will be on developing robust libraries, frameworks, and network
+        protocols. You have familiarity with the problem domain of scaling
+        arbitrary computation beyond a single machine and utilizing a
+        combination of cloud, on-prem and virtualization technologies. You
+        understand the challenges of maintaining an evolving product with
+        backward compatibility and wide platform support.
+      </p>
+      <br />
+      <h3 className="text-2xl">Skills & Qualifications</h3>
+      <ul style={{ listStyleType: 'circle' }}>
+        <li>
+          Expertise in Python with package development and metaprogramming
+          experience
+        </li>
+        <li>
+          Experience implementing distributed scalable job execution systems
+        </li>
+        <li>
+          Proficiency working with containers and virtualization technologies
+        </li>
+        <li>
+          Owning important technical decisions and tradeoffs in challenging
+          projects
+        </li>
+        <li>A track record of working in global / remote / async teams</li>
+        <li>
+          Excellent problem-solving skills and a passion for innovative
+          solutions
+        </li>
+        <li>
+          Ability to work productively without oversight during your team's time
+          zone
+        </li>
+        <li>The desire and capability to learn new things quickly as needed</li>
+        <li>
+          Effective communication skills and desire for honest feedback with
+          radical candor
+        </li>
+        <li>
+          Your unique background and ability to share your specialist knowledge
+          with the team
+        </li>
+      </ul>
+      <br />
+      <h3 className="text-2xl">Additional Skills of Strong Candidates</h3>
+      <ul style={{ listStyleType: 'circle' }}>
+        <li>CI / CD pipelines / DevOps / GitOps</li>
+        <li>Kubernetes, Docker Compose, Linux</li>
+        <li>Numpy, Pandas</li>
+        <li>Python Packaging</li>
+        <li>Remote Procedure Call / Serde</li>
+        <li>Python AST and Computational Graphs</li>
+        <li>Container / Network Security</li>
+        <li>Peer-to-Peer Networking</li>
+        <li>Decentralized identifiers (DIDs)</li>
+        <li>GPU Performance Optimization</li>
+        <li>Federated Learning</li>
+        <li>Differential Privacy</li>
+        <li>Secure Multi-Party Computation</li>
+        <li>Distributed Garbage Collection</li>
+        <li>Network Protocols / File Formats</li>
+        <li>Big Data / MapReduce</li>
+        <li>Vector Databases</li>
+        <li>Transformers / LLMs</li>
+        <li>Implementing Autograd</li>
+      </ul>
+      <br />
+      <h3 className="text-2xl">Responsibilities</h3>
+      <ul style={{ listStyleType: 'circle' }}>
+        <li>
+          Collaborate with our global teams to design and build innovative
+          features for PySyft
+        </li>
+        <li>
+          Engage in planning and pilot scoping with Tech Leads and the Product
+          Team
+        </li>
+        <li>
+          Enhance our development experience through in-house tooling, code
+          delivery optimization and continuous deployment
+        </li>
+        <li>Actively contribute to our diverse and inclusive team culture</li>
+        <li>
+          Deliver monthly feature sets, balancing delivery cadence and feature
+          scope effectively
+        </li>
+        <li>
+          Maintain our commitment to high-quality, open-source code on GitHub
+        </li>
+        <li>
+          Interact with team members from globally recognized partner
+          organizations to ensure product success
+        </li>
+      </ul>
+      <br />
+      <h3 className="text-2xl">Benefits</h3>
+      <ul style={{ listStyleType: 'circle' }}>
+        <li>
+          Opportunity to own high impact work on projects for globally
+          recognized organizations
+        </li>
+        <li>
+          Be part of a 501(c)(3) instead of a profit driven VC backed startup
+        </li>
+        <li>Enjoy the flexibility to work the hours that make sense for you</li>
+        <li>Work fully remote from wherever you want #LI-Remote</li>
+        <li>Contribute to an Open Source project in a public GitHub repo</li>
+        <li>Have an outsized impact on our rapidly growing organization</li>
+        <li>Experience freedom to work with the tools you prefer</li>
+        <li>
+          Work somewhere that fights organizational entropy and values getting
+          things done
+        </li>
+        <li>Take flexible vacation time</li>
+        <li>Enjoy local public holidays</li>
+        <li>We pay competitively</li>
+      </ul>
+
+      <br />
+      <p>
+        If your experience doesn't line up perfectly with the above, but are
+        mission driven, tenacious and excited about changing the world then
+        please apply!
+      </p>
+      <br />
+
+      <LinkButton href="/careers/software-engineer-form">Apply Now!</LinkButton>
+      <br />
+
+      <h3 className="text-2xl">OpenMined's Mission</h3>
+      <p>
+        Over the last 20 years, a handful of AI algorithms have come to directly
+        control over 12 billion hours a day of people's time, equating roughly
+        to 9.3% of the waking human experience.
+      </p>
+      <br />
+      <p>
+        Now, frontier AI systems are emerging with the potential to push beyond
+        this 9.3% and enhance or automate all intellectual labor around the
+        world with super intelligent capabilities. However, we are largely blind
+        to AI's true effects upon the world because researchers can't access
+        sufficient AI models or datasets at 1st, 2nd, or 3rd parties.
+      </p>
+      <br />
+      <p>
+        16,000+ members strong and 6+ years old, OpenMined is a non-profit on a
+        mission to help AI researchers understand and predict AI's impact on
+        society. By building AI governance infrastructure that helps researchers
+        access production AI systems and datasets — to study AI's impact on the
+        world.
+      </p>
+    </section>
+  );
+};
