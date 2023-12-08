2cf323311f9512036a181554c40256f4550c00a1
Madhava Jay <me@madhavajay.com>
2023-12-08 15:47:51 +1000

    Updated copy for job ad and cleaned up netlify references

--- src/routes/Careers.jsx
+++ src/routes/Careers.jsx
@@ -1,4 +1,5 @@
 import LinkButton from '../components/LinkButton';
+import logo_openmined from '../img/logo_openmined.png';
 
 export default () => {
   const positions = [
@@ -14,15 +15,24 @@ export default () => {
       <h2 className="font-bold mb-2">Careers</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {positions.map(({ title, location, department, link }) => (
-          <div className="flex flex-col gap-1 hover:opacity-80">
-            <h3>
-              <a href={link}>{title}</a>
-            </h3>
-            <p>{location}</p>
-            <p>{department}</p>
-            <span className="mt-4">
-              <LinkButton href={link}>More Info</LinkButton>
-            </span>
+          <div className="flex flex-row gap-3 hover:opacity-80">
+            <div className="flex-shrink-0">
+              <img
+                src={logo_openmined}
+                alt="OpenMined"
+                className="max-w-[50px]"
+              />
+            </div>
+            <div className="flex-grow">
+              <h3>
+                <a href={link}>{title}</a>
+              </h3>
+              <p>Location: {location}</p>
+              <p>Department: {department}</p>
+              <div className="pt-2">
+                <LinkButton href={link}>More Info</LinkButton>
+              </div>
+            </div>
           </div>
         ))}
       </div>
