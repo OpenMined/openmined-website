302d2e306c4c7abd35b027930c0512137fe320e7
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-02-16 14:16:18 +0000

    feat: add General Interest position

---
+++ src/routes/GIForm.jsx
@@ -0,0 +1,8 @@
+import HubspotForm from '../components/HubspotForm';
+
+export default () => (
+  <HubspotForm
+    hubspotFormId="540251a0-c802-498a-9aba-83f5f5ec6cbe"
+    formName="General Interest Application"
+  />
+);
