7ee6b4e24e87c4bc44b22b08a44b3815d37e2b44
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-05-04 17:55:07 +0200

    feat: cya later react

--- src/routes/GIForm.jsx
+++
@@ -1,8 +0,0 @@
-import HubspotForm from '../components/HubspotForm';
-
-export default () => (
-  <HubspotForm
-    hubspotFormId="540251a0-c802-498a-9aba-83f5f5ec6cbe"
-    formName="General Interest Application"
-  />
-);
