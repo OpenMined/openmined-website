4c606d66849045856e04c42c4a83a1a209cc344d
Thiago Costa Porto <thiagocostaporto@gmail.com>
2024-08-01 17:00:33 +0200

    feat: copy changes to community

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -1,18 +1,21 @@
 export default () => {
   const items = [
     {
+      heading: 'PySyft on GitHub',
       link: 'https://github.com/OpenMined/pysyft',
       text: "Check out our flagship software PySyft on Github: Perform data science on data that remains in someone else's server",
-      buttonText: 'PySyft on GitHub',
+      buttonText: 'Install PySyft',
     },
     {
+      heading: 'Documentation',
       link: 'https://docs.openmined.org/',
       text: 'Get started with PySyft: Unlock your data in less than 10 minutes',
       buttonText: 'Quickstart',
     },
     {
+      heading: "OpenMined's Blog",
       link: 'https://blog.openmined.org',
-      text: "Discover the latest updates on PySyft in OpenMined's blog post",
+      text: "Discover the latest updates on PySyft in OpenMined's blog",
       buttonText: 'Latest updates',
     },
   ];
@@ -20,9 +23,10 @@ export default () => {
   return (
     <section className="flex flex-col px-6 max-w-[1152px] mx-auto mb-16 md:mb-[144px]">
       <h2 className="font-bold mb-2">Learn about PySyft</h2>
-      <div id="learn" className="grid grid-cols-1 md:grid-cols-3 gap-12">
-        {items.map(({ link, text, buttonText }) => (
-          <div className="border-t border-black flex flex-col flex-grow pt-4 gap-2">
+      <div id="learn" className="grid grid-cols-1 md:grid-cols-3 gap-12 border-t border-black">
+        {items.map(({ heading, link, text, buttonText }) => (
+          <div className="flex flex-col flex-grow pt-4 gap-2">
+            <h3>{heading}</h3>
             <p className="flex-grow">{text}</p>
             <a
               target="_blank"
